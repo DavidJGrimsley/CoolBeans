ae80507dc6fd847991d436dc382f905fff53c0da
DavidJGrimsley <114373822+DavidJGrimsley@users.noreply.github.com>
2022-09-25 23:42:24 -0400

    Initial Commit

---
+++ CoolBeans/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CoolBeans
+//
+//  Created by DJ on 7/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var menu = Menu()
+    
+    var body: some View {
+        MenuView()
+            .environmentObject(menu)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
